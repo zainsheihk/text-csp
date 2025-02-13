bcb2088fad2d693bd66f4e670252d9f495724f7e
fahadiqbal <fahadiqbal0332@gmail.com>
2025-02-13 16:01:06 +0500

    chnages in headers

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,8 @@ export function middleware(request: NextRequest) {
 
   const cspHeader = `
         default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
+    script-src 'self' 'nonce-${nonce}' https://text-csp.vercel.app 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce} https://text-csp.vercel.app/';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
